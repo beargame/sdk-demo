2e98bb3291331844125420daa88c92286d6a6c4c
nodephp <nodephp@gmail.com>
2016-07-18 14:39:56 +0800

    Update pay_notify.php

--- php/pay_notify.php
+++ php/pay_notify.php
@@ -1,17 +1,24 @@
 <?php
 $orderJson = json_encode($_POST);
+$appSecret = YOUR_APP_SECRET;
 
 $sign = $_POST['sign'];
-
 $arr = $_POST;
 unset($arr['sign']);
 ksort($arr);
-$mySign = md5(http_build_query($arr) . "b3ad67fa6b25d435d4dd12896005428f");
+
+foreach ($arr as $key => $value) {
+	$params[] = $key . '=' . $value;
+}
+$str = implode("&", $params);
+
+$mySign = md5($str . YOUR_APP_SECRET);
 if ($sign == $mySign) {
 	$ret = 1;
+	echo "OK";
 } else {
 	$ret = 0;
+	echo "FAIL";
 }
 
 error_log(date("Y-m-d H:i:s") . "\t" . $orderJson . "\t" . $ret . "\n", 3, '/data/logs/php/wx_back.log');
-echo "OK";
